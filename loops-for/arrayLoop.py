2b0a5f89c9c664baad14685baea3e833290aac1e
rust333m <47534213+rust333m@users.noreply.github.com>
2020-08-04 17:14:23 +0600

    Update arrayLoop.py

--- loops-for/arrayLoop.py
+++ loops-for/arrayLoop.py
@@ -1,6 +1,7 @@
 def insert_squares(arr, num):
-    	for i in arr:
-		print(i)
+    	arr = [1,2,3]
+	arr2 = []
 	for i in range(1,num+1):
-		print(i*i)
+		arr2.append(i*i)
+	print(arr + arr2)
     # add square of numbers from 1 to num to the list named arr and return list
