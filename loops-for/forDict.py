3e92685f2b8248baae2c4a69714a76b8af1a5255
rust333m <47534213+rust333m@users.noreply.github.com>
2020-08-09 19:01:04 +0600

    Update forDict.py

--- loops-for/forDict.py
+++ loops-for/forDict.py
@@ -1,6 +1,6 @@
 def reverse_dict(dict):
     # swap keys and values within dict and return dict
-    dict1 = dict{}
+    dict1 = dict()
     for key in dict:
         dict1[dict[key]] = key
         
