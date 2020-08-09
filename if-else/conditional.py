9a6647d34d0256f904ed8e7025edd194b16fe590
rust333m <47534213+rust333m@users.noreply.github.com>
2020-08-09 14:00:04 +0600

    Update conditional.py

--- if-else/conditional.py
+++ if-else/conditional.py
@@ -1,16 +1,32 @@
 import random
 
 def is_positive(num):
+    if num >= 0:
+        return True
+    else:
+        return False
     # return true if num is positive, otherwise return false
 
 def is_even(num):
     # return true if num is even, otherwise return false
+    if num % 2 == 0:
+        return True
+    else:
+        return False
 
 
 def is_positive_and_even(num):
     # return true if num is positive and even, otherwise return false
+    if num % 2 == 0 and num >= 0:
+        return True
+    else:
+        return False
 
 
 def is_positive_or_even(num):
     # return true if num is positive or even, otherwise return false
+    if num % 2 == 0 or num >= 0:
+        return True
+    else:
+        return False
 
